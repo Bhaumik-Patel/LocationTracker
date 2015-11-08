bc87d278005b0c82ffc3b55083c053e02cd38146
Bhaumik-Patel <bhaumikpatel1609@yahoo.co.in>
2015-11-08 16:30:54 +1100

    Test

--- server.js
+++ server.js
@@ -1,37 +1,32 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req,res)
-{
-	res.send("Hello");
+app.use(express.static(__dirname + "/public"))
+app.get('/location', function(req, res){
+
+	console.log('Get Request')
+
+	var satelize = require('satelize');
+ 
+// Example retrieve IP from request 
+ var ip = req.header('x-forwarded-for') || req.connection.remoteAddress; 
+ 
+// then satelize call  
+ 
+satelize.satelize(ip, function(err, geoData) {
+  // process err 
+  
+  // if data is JSON, we may wrap it in js object 
+  var obj = JSON.parse(geoData);
+  res.json(obj);
+  
+  // if used with expressjs 
+  // res.send(geoData); 
+  // res.json... 
 });
 
-// app.use(express.static(__dirname + "/public"))
-// app.get('/location', function(req, res){
 
-// 	console.log('Get Request')
-
-// 	var satelize = require('satelize');
- 
-// // Example retrieve IP from request 
-//  var ip = req.header('x-forwarded-for') || req.connection.remoteAddress; 
- 
-// // then satelize call  
- 
-// satelize.satelize(ip, function(err, geoData) {
-//   // process err 
-  
-//   // if data is JSON, we may wrap it in js object 
-//   var obj = JSON.parse(geoData);
-//   res.json(obj);
-  
-//   // if used with expressjs 
-//   // res.send(geoData); 
-//   // res.json... 
-// });
-
-
-// });
+});
 
 
 app.listen(3000);

b378b554517dd1ea837e836a10d109e677ba7c59
Bhaumik-Patel <bhaumikpatel1609@yahoo.co.in>
2015-11-08 21:12:15 +1100

    test

--- server.js
+++ server.js
@@ -1,18 +1,68 @@
-var express = require('express');
-var app = express();
+/************************************************
+FILENAME
+server_simple.js
+DESCRIPTION
+creates a simple web server that
+display "Hello Dynamic World Wide Web"
+HOW TO START SERVER:
+1) from terminal run 'node simple_server.js'
+2) open web browser visit http://127.0.0.1:8080
+*************************************************/
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+// Include the HTTP Node library
+// http://nodejs.org/docs/latest/api/http.html
+var http = require('http');
 
-});
+// define the IP and port number
+var localIP = "127.0.0.1"; // 127.0.0.1 is used when running the server locally
+var port = 8080; // port to run webserver on
 
-var server = app.listen(1337, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+function sayHello(req, res) {
+    
+    console.log("We've got a request for " + req.url);
+    
+    // HTTP response header - the content will be HTML MIME type
+    res.writeHead(200, {'Content-Type': 'text/html'});
 
-  console.log('Example app listening at http://%s:%s', host, port);
+    // Write out the HTTP response body
+    res.write('<html><body>' +
+    '<h1>Hello Dynamic World Wide Web<h1>'+
+    '</body></html>');
+    
+    // End of HTTP response
+    res.end();
+    
+}
 
-});
+/************************/
+/*  START THE SERVER    */
+/************************/
+
+// Create the HTTP server
+var server = http.createServer(sayHello);
+
+// Turn server on - now listening for requests on localIP and port
+server.listen(port, localIP);
+
+// print message to terminal that server is running
+console.log('Server running at http://'+ localIP +':'+ port +'/');
+
+
+// var express = require('express');
+// var app = express();
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+
+// });
+
+// var server = app.listen(3000, function () {
+//   var host = server.address().address;
+//   var port = server.address().port;
+
+//   console.log('Example app listening at http://%s:%s', host, port);
+
+// });
 
 
 // var express = require('express');

c0222f1e0a4543e002640aaf0c1a29769ce30071
Bhaumik-Patel <bhaumikpatel1609@yahoo.co.in>
2015-11-08 22:15:54 +1100

    test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.static(__dirname + "/public"))
 
 //app.listen(1337, "127.0.0.1");
-app.listen(1337, "162.243.230.213");
+app.listen(1337);
 console.log("server running");
 
 app.get('/location', function(req, res){

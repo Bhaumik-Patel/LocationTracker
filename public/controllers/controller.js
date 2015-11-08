76d347cab13fe4f471088e46bcdc3ec21e65e490
Bhaumik-Patel <bhaumikpatel1609@yahoo.co.in>
2015-11-08 13:11:04 +1100

    Upload application

---
+++ public/controllers/controller.js
@@ -0,0 +1,16 @@
+var myApp = angular.module('myApp',[]);
+
+var AppCtrl = function($scope, $http){
+		$scope.GetLocation = function() {
+       $http.get('/location').success(function(response){
+		console.log('Got response');
+		$scope.location = response;
+	})
+       .error(function(reason)
+       {
+       		$scope.error = "Could not fetch the data.";
+       });
+    };
+};
+
+myApp.controller('AppCtrl', AppCtrl);
---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + "/public"))
+app.get('/location', function(req, res){
+
+	console.log('Get Request')
+
+	var satelize = require('satelize');
+ 
+// Example retrieve IP from request 
+ var ip = req.header('x-forwarded-for') || req.connection.remoteAddress; 
+ 
+// then satelize call  
+ 
+satelize.satelize(ip, function(err, geoData) {
+  // process err 
+  
+  // if data is JSON, we may wrap it in js object 
+  var obj = JSON.parse(geoData);
+  res.json(obj);
+  
+  // if used with expressjs 
+  // res.send(geoData); 
+  // res.json... 
+});
+
+
+});
+
+
+app.listen(1337);
+console.log("server running");
